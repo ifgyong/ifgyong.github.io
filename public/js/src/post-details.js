91cf07a1e9195846bdf61257fd3ce983030ea023
FanGuangYong <fgyong@yeah.net>
2017-11-01 17:33:26 +0800

    add files

--- public/js/src/post-details.js
+++ public/js/src/post-details.js
@@ -118,6 +118,7 @@ $(document).ready(function () {
     item.addClass(activeTabClassName);
   });
 
+  // TOC item animation navigate & prevent #item selector in adress bar.
   $('.post-toc a').on('click', function (e) {
     e.preventDefault();
     var targetSelector = NexT.utils.escapeSelector(this.getAttribute('href'));
@@ -134,13 +135,14 @@ $(document).ready(function () {
   });
 
   // Expand sidebar on post detail page by default, when post has a toc.
-  NexT.motion.middleWares.sidebar = function () {
-    var $tocContent = $('.post-toc-content');
-
-    if (CONFIG.sidebar.display === 'post' || CONFIG.sidebar.display === 'always') {
-      if ($tocContent.length > 0 && $tocContent.html().trim().length > 0) {
-        NexT.utils.displaySidebar();
-      }
-    }
-  };
+  var $tocContent = $('.post-toc-content');
+  var isSidebarCouldDisplay = CONFIG.sidebar.display === 'post' ||
+      CONFIG.sidebar.display === 'always';
+  var hasTOC = $tocContent.length > 0 && $tocContent.html().trim().length > 0;
+  if (isSidebarCouldDisplay && hasTOC) {
+    CONFIG.motion ?
+      (NexT.motion.middleWares.sidebar = function () {
+          NexT.utils.displaySidebar();
+      }) : NexT.utils.displaySidebar();
+  }
 });
